65ec1e01ac18a164f2727dcf51061ca09bb7062f
Waer1 <elwaeryousef@gmail.com>
2023-05-05 08:34:32 +0000

    done running the models in the cloud

--- run_models.sh
+++ run_models.sh
@@ -12,8 +12,10 @@ do
   for model in {0..3}
   do
     container_name="feature-${feature}-model-${model}"
-    docker run --name "${container_name}" -e "VAR1=${feature}" -e "VAR2=${model}" -v "${dataset_dir}:/train/dataset" THEIMAGE > "${output_dir}/${container_name}.txt" 2>&1 &
+    docker run --name "${container_name}" -e "FEATURE_METHOD=${feature}" -e "MODEL_METHOD=${model}" -v "${dataset_dir}:/train/dataset" THEIMAGE > "${output_dir}/${container_name}.txt" 2>&1 &
   done
 done
 
 wait
+
+    
