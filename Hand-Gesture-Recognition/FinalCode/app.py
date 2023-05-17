13acef1b46e8d815cbade2bb995e2c9b9c022064
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-05-17 23:28:08 +0300

    update submitted files

---
+++ Hand-Gesture-Recognition/FinalCode/app.py
@@ -0,0 +1,52 @@
+import pickle
+import cv2
+import os
+import time
+import numpy as np
+from preprocessing import preprocess
+from features import hog_features
+
+# Load the trained model from pickle file
+with open('classifier.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Create the time and results txt files
+resultsFile = open("results.txt", "w")
+timeFile = open("time.txt", "w")
+
+# get all the image names
+dataset = "./data/"
+images = os.listdir(dataset)
+
+# Hog parameters
+orientations = 9
+pixels_per_cell = (8, 8)
+cells_per_block = (2, 2)
+
+# iterate over the image names, get the label
+for image in images:
+    image_path = dataset + image
+    image = cv2.imread(image_path)
+
+    # Start the timer
+    start_time = time.time()
+
+    try:
+        # Preprocessing phase
+        image = preprocess(image)
+
+        # Feature extraction phase
+        feature = np.array(hog_features(image, orientations=orientations,
+                           pixels_per_cell=pixels_per_cell, cells_per_block=cells_per_block)).reshape(1, -1)
+
+        # Make prediction and output to the file
+        predicted_class = model.predict(feature)
+
+        resultsFile.write(f"{predicted_class[0]}\n")
+
+        # Finish the timer and output to the file
+        end_time = time.time()
+        timeFile.write(f"{round(end_time - start_time, 3)}\n")
+    except:
+        resultsFile.write(f"Error in image {image_path}\n")
+        timeFile.write(f"Error in image {image_path}\n")
---
+++ Hand-Gesture-Recognition/FinalCode/optimizeHog.py
@@ -0,0 +1,135 @@
+
+# Imports
+import cv2
+from preprocessing import preprocess
+from models import svm
+from features import hog_features
+from sklearn.metrics import accuracy_score
+import os
+
+# get all the image folder paths
+dataset = "./Dataset/"
+image_paths = os.listdir(dataset)
+
+
+def run_single(orientation, pixels_per_cell, cells_per_block, kernel, C, gamma):
+    feature_arr = []
+    label_arr = []
+
+    for path in image_paths:
+        # get all the image names
+        images = os.listdir(dataset + path)
+
+        # iterate over the image names, get the label
+        for image in images:
+            image_path = dataset + f"{path}/{image}"
+            try:
+                image = cv2.imread(image_path)
+
+                # Preprocessing phase
+                image = preprocess(image)
+
+                # Feature extraction phase
+                feature = hog_features(image, orientations=orientation,
+                                       pixels_per_cell=pixels_per_cell, cells_per_block=cells_per_block)
+
+                # update the data and labels
+                feature_arr.append(feature)
+                label_arr.append(path)
+            except:
+                print(image_path)
+
+    model, X_test, y_test = svm(feature_arr, label_arr, kernel, C, gamma)
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Orientation: {orientation}, Pixels per cell: {pixels_per_cell}, Cells per block: {cells_per_block}, Kernel: {kernel}, C: {C}, Gamma: {gamma}, Accuracy: {accuracy}")
+    return accuracy
+
+
+def run_HOG_SVM(orientations_list, pixels_per_cell_list, cells_per_block_list, kernel_list, C_list, gamma_list):
+    best_accuracy = 0
+    best_params = {}
+    accuracy = 0
+
+    best_params['gamma'] = gamma_list[0]
+    best_params['C'] = C_list[0]
+    best_params['kernel'] = kernel_list[0]
+    best_params['cells_per_block'] = cells_per_block_list[0]
+    best_params['pixels_per_cell'] = pixels_per_cell_list[0]
+    best_params['orientations'] = orientations_list[0]
+
+    for gamma in gamma_list:
+        accuracy = run_single(orientations_list[0], pixels_per_cell_list[0],
+                              cells_per_block_list[0], kernel_list[0], C_list[0], gamma)
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_params['gamma'] = gamma
+
+    print("the best gamma parameters were: ", best_params['gamma'])
+
+    for c_val in C_list:
+        accuracy = run_single(orientations_list[0], pixels_per_cell_list[0],
+                              cells_per_block_list[0], kernel_list[0], c_val, best_params['gamma'])
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_params['C'] = gamma
+
+    print("the best C parameters were: ", best_params['C'])
+
+    for kernal_val in kernel_list:
+        accuracy = run_single(orientations_list[0], pixels_per_cell_list[0],
+                              cells_per_block_list[0], kernal_val, best_params['C'], best_params['gamma'])
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_params['kernel'] = kernal_val
+
+    print("the best kernel parameters were: ", best_params['kernel'])
+
+    for cell_per_block_val in cells_per_block_list:
+        accuracy = run_single(orientations_list[0], pixels_per_cell_list[0], cell_per_block_val,
+                              best_params['kernel'], best_params['C'], best_params['gamma'])
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_params['cells_per_block'] = cell_per_block_val
+
+    print("the best cells_per_block parameters were: ",
+          best_params['cells_per_block'])
+
+    for pixels_per_cell_val in pixels_per_cell_list:
+        accuracy = run_single(orientations_list[0], pixels_per_cell_val, best_params['cells_per_block'],
+                              best_params['kernel'], best_params['C'], best_params['gamma'])
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_params['pixels_per_cell'] = pixels_per_cell_val
+
+    print("the best pixels_per_cell parameters were: ",
+          best_params['pixels_per_cell'])
+
+    for orientations_val in orientations_list:
+        accuracy = run_single(orientations_val, best_params['pixels_per_cell'], best_params[
+                              'cells_per_block'],  best_params['kernel'], best_params['C'], best_params['gamma'])
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_params['orientations'] = orientations_val
+
+    print("the best orientations parameters were: ",
+          best_params['orientations'])
+
+    return best_accuracy, best_params
+
+
+orientations_list = [9]
+pixels_per_cell_list = [(8, 8)]  # (8,8)
+cells_per_block_list = [(2, 2)]  # (2,2)
+kernel = ["poly"]
+C = [0.5]
+gamma = ["scale"]
+
+
+accuracy = run_single(
+    orientations_list[0], pixels_per_cell_list[0], cells_per_block_list[0], kernel[0], C[0], gamma[0])
+
+print(f"Accuracy: {accuracy}")
+
+# print(f"Best accuracy: {best_accuracy}")
+# print(f"Best params: {best_params}")
